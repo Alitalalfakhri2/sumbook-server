2eca030b2342b8abf378b0e2efdb21c2dd908097
Alitalalfakhri2 <ali.talalfakhri0009@gmail.com>
2025-06-22 01:35:27 +0300

    Update index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express();
 
 const corsOptions = {
-  origin: "https://c5d09a34-a349-452a-9793-8770b639ec47-00-kz54gc95g93g.pike.replit.dev",
+  origin: "https://sumbook-client.vercel.app",
   credentials: true, // Allow sending cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
